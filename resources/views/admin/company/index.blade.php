f41c0ef9309f53060a5f4ab52bb329ea7e36d6e2
Ramon <ramonlima08@gmail.com>
2020-12-23 16:56:43 -0300

    Comeco de criacao

---
+++ resources/views/admin/company/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title',"Gestão de Produtos")
+
+@section('content')
+
+    <h1>Empresa</h1>
+
+    <a href="{{ route('empresa.create') }}" class="btn btn-primary"> Cadastrar Empresa </a>
+    
+    
+    
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('welcome');
+})->name('login');
+
 //Auth::routes(['register'=>false]);
 
 Route::group([
@@ -23,11 +16,15 @@ Route::group([
     'prefix' => 'admin'
 ], function(){
     Route::get('/dashboard', 'Controller@dashboard')->name('admin.dashboard');
-    Route::resource('/contato', 'Contact');
-    Route::resource('/empresa', 'Company');
+    Route::resource('/contato', 'Admin\ContactController');
+    Route::resource('/empresa', 'Admin\CompanyController');
+    Route::resource('/agenda', 'Admin\ScheduleController');
     
     //aqui é um caso de redirecionamento para o dashboard
     Route::get('/', function(){
         return redirect()->route('admin.dashboard');
     })->name('admin.home');
+    Route::get('/home', function(){
+        return redirect()->route('admin.dashboard');
+    })->name('admin.home2');
 });
